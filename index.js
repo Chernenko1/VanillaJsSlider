6a2ce3062fb05ed8e6a382b417fce725817c354b
Chernenko1 <kchek816@gmail.com>
2025-02-27 14:49:52 +0300

    feat: anim

--- index.js
+++ index.js
@@ -7,25 +7,51 @@ let currentSlide = 0
 
 let slidersCount = sliders.length - 1
 
-function changeSlide () {
-  for(let i = 0; i <= slidersCount; i++) {
-    sliders[i].classList.remove('active')
-    navdots[i].classList.remove('active')
-  }
+function changeNextSlide () {
   sliders[currentSlide].classList.add('active')
   navdots[currentSlide].classList.add('active')
+
+  setTimeout(() => {  
+    sliders[(currentSlide === 0) ? slidersCount : currentSlide - 1].classList.remove('active')
+    nextBtn.disabled  = false; 
+  }, 500)
+  navdots[(currentSlide === 0) ? slidersCount : currentSlide - 1].classList.remove('active')
+  // for(let i = 0; i <= slidersCount; i++) {
+  //   sliders[i].classList.remove('active')
+  //   navdots[i].classList.remove('active')
+  // }
+}
+
+function changePrevSlide () {
+  sliders[currentSlide].classList.add('active')
+  navdots[currentSlide].classList.add('active')
+
+  sliders[(currentSlide === 4) ?  0 : currentSlide + 1].classList.remove('active')
+  prevBtn.disabled = true;
+  navdots[(currentSlide === 4) ? 0 : currentSlide + 1].classList.remove('active')
+  // for(let i = 0; i <= slidersCount; i++) {
+  //   sliders[i].classList.remove('active')
+  //   navdots[i].classList.remove('active')
+  // }
 }
 
 function setNextSlide () {
-  (currentSlide === slidersCount) ? currentSlide = 0 :      currentSlide++;
-  changeSlide()
+  (currentSlide === slidersCount) ? currentSlide = 0 : currentSlide++;
+  changeNextSlide()
 }
 
 function setPrevSlide () {
-  (currentSlide === 0) ? currentSlide = slidersCount :      currentSlide--;
-  changeSlide()
+  (currentSlide === 0) ? currentSlide = slidersCount : currentSlide--;
+  changePrevSlide()
   
 }
 
-nextBtn.addEventListener('click', () => setNextSlide())
-prevBtn.addEventListener('click', () => setPrevSlide())
+nextBtn.addEventListener('click', () => {
+  nextBtn.disabled  = true; 
+  setNextSlide()
+})
+
+prevBtn.addEventListener('click', () => {
+  prevBtn.disabled = true; 
+  setPrevSlide()
+})
